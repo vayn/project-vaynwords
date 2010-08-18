5d019e22337ae20652776c2b3ae3ede4d33d47e9
vayn <vt@elnode.com>
2010-08-18 15:07:10 +0800

    change arrows

--- footer.php
+++ footer.php
@@ -31,6 +31,6 @@
         </div>
     </div>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript" src="./js/wp-javascription.js"></script>
+<script type="text/javascript" src="./js/shawnjs.js"></script>
 </body>
 </html>
