8bf66bb3ad3293e39ee5d2ee458abd05813edf19
vayn <vt@elnode.com>
2010-10-03 22:40:12 +0800

    fixed mobile footer

--- mobile/footer.php
+++ mobile/footer.php
@@ -3,7 +3,7 @@
     <ul class="entry button" id="x-button-get-entry">
         <li>
         <?php if ($_SERVER['QUERY_STRING']) { ?>
-            <div align="center"><?php echo VWSCore::pagination(); ?></div>
+            <div align="center"><?php echo VWSCore::pagination('/mobile/'); ?></div>
         <?php } else { ?>
             <div class="x-button" align="center" onclick="getEntry();">载入更多</div>
         <?php } ?>
